e027967152a2becf4a2967ede70a274ad3dc2e06
Matthew Lalmansingh <gpjazzplayer@gmail.com>
2018-09-09 18:39:16 -0500

    Add click listener to Main Activity layout
    - Add logic to make entire landing Activity clickable

--- app/src/main/java/com/app/keeperofthevoid/quizapp/MainActivity.kt
+++ app/src/main/java/com/app/keeperofthevoid/quizapp/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.app.keeperofthevoid.quizapp
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
+import android.support.v7.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<ConstraintLayout>(R.id.main_activity).setOnClickListener { println("tap") }
     }
 }
