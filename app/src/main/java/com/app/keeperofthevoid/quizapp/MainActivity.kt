c408ec31f55077071f2a153d6d811ab1024456f2
Matthew Lalmansingh <gpjazzplayer@gmail.com>
2018-09-09 19:02:10 -0500

    Add Base Score Activity
    - Add Score Activity
    - Add Background Color for Score Activity
    - Add Intent to Navigate from Landing to Score Activity

--- app/src/main/java/com/app/keeperofthevoid/quizapp/MainActivity.kt
+++ app/src/main/java/com/app/keeperofthevoid/quizapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.app.keeperofthevoid.quizapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.constraint.ConstraintLayout
 import android.support.v7.app.AppCompatActivity
@@ -10,6 +11,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<ConstraintLayout>(R.id.main_activity).setOnClickListener { println("tap") }
+        findViewById<ConstraintLayout>(R.id.main_activity).setOnClickListener { startQuiz() }
+    }
+
+    private fun startQuiz() {
+
+        // Create an Intent to start the second activity
+        val quizIntent = Intent(this, ScoreActivity::class.java)
+        startActivity(quizIntent)
     }
 }
